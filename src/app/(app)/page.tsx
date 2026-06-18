f423b9719aef929c580454d48a066974a860273d
Uthman AHMAD <uthmana.muhammad@gmail.com>
2026-06-18 15:59:16 +0300

    chore: update gitignore file

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,38 +4,40 @@ export default async ({
   params: Promise<{ slug: string[] }>;
 }) => {
   return (
-    <div>
-      <h1>Multi-Tenant | Core CMS </h1>
-      <p>
-        This Multi-Tenant Core CMS allows you to manage multi cms project with
-        domains and with slugs.
-      </p>
+    <section className="home-page">
+      <div>
+        <h1>Multi-Tenant | Core CMS </h1>
+        <p>
+          This Multi-Tenant Core CMS allows you to manage multi cms project with
+          domains and with slugs.
+        </p>
 
-      <h2>Domains</h2>
-      <p>
-        When you visit a tenant by domain, the domain is used to determine the
-        tenant.
-      </p>
-      <p>
-        For example, visiting{" "}
-        <a href="http://gold.localhost:3000/tenant-domains/login">
-          http://gold.localhost:3000/tenant-domains/login
-        </a>{" "}
-        will show the tenant with the domain "gold.localhost".
-      </p>
+        <h2>Domains</h2>
+        <p>
+          When you visit a tenant by domain, the domain is used to determine the
+          tenant.
+        </p>
+        <p>
+          For example, visiting{" "}
+          <a href="http://gold.localhost:3000/tenant-domains/login">
+            http://gold.localhost:3000/tenant-domains/login
+          </a>{" "}
+          will show the tenant with the domain "gold.localhost".
+        </p>
 
-      <h2>Slugs</h2>
-      <p>
-        When you visit a tenant by slug, the slug is used to determine the
-        tenant.
-      </p>
-      <p>
-        For example, visiting{" "}
-        <a href="http://localhost:3000/tenant-slugs/silver/login">
-          http://localhost:3000/tenant-slugs/silver/login
-        </a>{" "}
-        will show the tenant with the slug "silver".
-      </p>
-    </div>
+        <h2>Slugs</h2>
+        <p>
+          When you visit a tenant by slug, the slug is used to determine the
+          tenant.
+        </p>
+        <p>
+          For example, visiting{" "}
+          <a href="http://localhost:3000/tenant-slugs/silver/login">
+            http://localhost:3000/tenant-slugs/silver/login
+          </a>{" "}
+          will show the tenant with the slug "silver".
+        </p>
+      </div>
+    </section>
   );
 };
